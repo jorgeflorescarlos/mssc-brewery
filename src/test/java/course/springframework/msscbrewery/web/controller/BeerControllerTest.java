1cd9b15947a97e0f8840ed81235db7d3a5942143
Jorge Flores Carlos <florescarlos167@gmail.com>
2023-09-26 15:48:36 -0600

    adding request mapping example and test

---
+++ src/test/java/course/springframework/msscbrewery/web/controller/BeerControllerTest.java
@@ -0,0 +1,98 @@
+package course.springframework.msscbrewery.web.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import course.springframework.msscbrewery.services.BeerService;
+import course.springframework.msscbrewery.web.model.BeerDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Created by jorge on 26/09/23
+ */
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    @MockBean
+    BeerService beerService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    BeerDto validBeer;
+
+    String apiEndpoint = "/api/v1/beer/";
+
+    @BeforeEach
+    void setUp() {
+        validBeer = BeerDto
+                .builder()
+                .id(UUID.randomUUID())
+                .beerName("Beer1")
+                .beerStyle("PALE_ALE")
+                .upc(123456789012L)
+                .build();
+    }
+
+    @Test
+    void getBeer() throws Exception {
+        given(beerService.getBeerById(any(UUID.class))).willReturn(validBeer);
+
+        mockMvc.perform(get(apiEndpoint + validBeer.getId().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.id", is(validBeer.getId().toString())))
+                .andExpect(jsonPath("$.beerName", is("Beer1")));
+    }
+
+    @Test
+    void handlePost() throws Exception {
+        BeerDto beerDto = validBeer;
+        beerDto.setId(null);
+
+        BeerDto savedDto = BeerDto
+                .builder()
+                .id(UUID.randomUUID())
+                .beerName("New Beer")
+                .build();
+        String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+        given(beerService.saveNewBeer(any())).willReturn(savedDto);
+
+        mockMvc.perform(post(apiEndpoint)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(beerDtoJson))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void handleUpdate() throws Exception {
+        //given
+        BeerDto beerDto = validBeer;
+        String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+        //when
+        mockMvc.perform(put(apiEndpoint + validBeer.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(beerDtoJson))
+                .andExpect(status().isNoContent());
+
+        then(beerService).should().updateBeer(any(), any());
+    }
+}
